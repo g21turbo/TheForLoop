86bc3395ade6cefed3908c072be1a1b3249c4eed
g21turbo <dg21stew@gmail.com>
2023-10-27 20:13:51 -0700

    Added third for loop

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main {
             System.out.println("10,000 at " + rate + "% interest = " + interestAmount);
         }
 
+        for (double rate = 7.5; rate <= 10.0; rate+=.25) {
+            double interestAmount = calculateInterest(100.00, rate);
+            System.out.println("100 at " + rate + "% interest = " + interestAmount);
+        }
+
     }
 
     public static double calculateInterest(double amount, double interestRate) {
