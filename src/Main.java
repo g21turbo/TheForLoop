d7ab4d794b65ec027aa90c093507784867e9eeb6
g21turbo <dg21stew@gmail.com>
2023-10-27 22:28:13 -0700

    Added break to third for loop

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,21 @@ public class Main {
             System.out.println(counter);
         }
 
+        System.out.println();
+
         for (double rate = 2.0; rate <= 5.0; rate++) {
-            double interestAmount = calculateInterest(1000.0, rate);
+            double interestAmount = calculateInterest(10000.0, rate);
             System.out.println("10,000 at " + rate + "% interest = " + interestAmount);
         }
 
+        System.out.println();
+
         for (double rate = 7.5; rate <= 10.0; rate+=.25) {
             double interestAmount = calculateInterest(100.00, rate);
-            System.out.println("100 at " + rate + "% interest = " + interestAmount);
+            if (interestAmount > 8.5) {
+                break;
+            }
+            System.out.println("$100 at " + rate + "% interest = " + interestAmount);
         }
 
     }
